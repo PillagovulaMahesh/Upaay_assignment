992e61d4d8f19d3bdeb94f4eade5890e77e56edc
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-09-20 16:46:25 +0530

    Update EmptyState.jsx

---
+++ Upaay_assignment/src/components/EmptyState.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmptyState = () => {
+  return (
+    <div className="text-gray-400 text-center py-6 italic">
+      No tasks here...
+    </div>
+  );
+};
+
+export default EmptyState;
